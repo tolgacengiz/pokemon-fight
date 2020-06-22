95dafc1b4d64056e7747d5d3f27ac86dbbcded5e
tolgacengiz <64279187+tolgacengiz@users.noreply.github.com>
2020-06-22 14:46:28 +0200

    Id & Info added

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -4,7 +4,35 @@ const pokedex = require('./pokedex.json');
 
 const pokedexRouter = express.Router();
 
-pokedexRouter.get('/', (req, res) => {
+pokedexRouter.get('/:id/:info', (req, res, next) => {
+    const { id, info } = req.params;
+    const idAsNumber = parseInt(id, 10);
+
+    const foundPokemon = pokedex.find((singlepokemon) => {
+        return singlepokemon.id === idAsNumber;
+    });
+
+    res.send(foundPokemon[info]);
+});
+
+pokedexRouter.get('/:id', (req, res, next) => {
+    const { id } = req.params;
+    const idAsNumber = parseInt(id, 10);
+
+    const foundPokemon = pokedex.find((singlepokemon) => {
+        return singlepokemon.id === idAsNumber;
+    });
+
+    res.send(foundPokemon);
+});
+
+pokedexRouter.get('/', (req, res, next) => {
+    const { id } = req.params;
+    const idAsNumber = parseInt(id, 10);
+
+    const foundPokemon = pokedex.find((singlepokemon) => {
+        return singlepokemon.id === idAsNumber;
+    });
 
     res.send(pokedex);
 });
