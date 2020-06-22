98cdabc9143dc3627eaac12875be9e31836e37fd
tolgacengiz <64279187+tolgacengiz@users.noreply.github.com>
2020-06-22 16:04:58 +0200

    updated package

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -27,12 +27,6 @@ pokedexRouter.get('/:id', (req, res, next) => {
 });
 
 pokedexRouter.get('/', (req, res, next) => {
-    const { id } = req.params;
-    const idAsNumber = parseInt(id, 10);
-
-    const foundPokemon = pokedex.find((singlepokemon) => {
-        return singlepokemon.id === idAsNumber;
-    });
 
     res.send(pokedex);
 });
