43e9113b44a3b4c9617d110f2058bc08cba9c482
tolgacengiz <64279187+tolgacengiz@users.noreply.github.com>
2020-06-29 16:09:38 +0200

    Secure news post route

--- routes/news.js
+++ routes/news.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const md5 = require('md5');
 const router = express.Router();
 const pool = require('../db-connector/pool');
 
@@ -25,17 +26,35 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     const { title, brief_text, detailed_text } = req.body;
 
-    pool
-        .query(
-            'INSERT INTO news (title, brief_text, detailed_text) VALUES ($1, $2, $3)',
-            [title, brief_text, detailed_text]
-        )
-        .then(data => {
-            res.send(data.rows)
-        })
-        .catch(err => {
-            res.send({ error: err.message })
-        });
+    // console.log(req.headers.authorization)
+
+    if (!req.headers.authorization) {
+        return res.send({ error: 'Unauthorized' })
+    }
+
+    const [encodedPayload, signature] = req.headers.authorization.replace('Bearer ', '').split('.');
+
+    const secretForToken = 'SECRET_FOR_TOKEN';
+    const generatedSignature = md5(encodedPayload + secretForToken); // Second part of the token
+
+    if (signature === generatedSignature) {
+        // here we would also check if user from encodedPayload
+        // is allowed to post the data
+
+        pool
+            .query(
+                'INSERT INTO news (title, brief_text, detailed_text) VALUES ($1, $2, $3)',
+                [title, brief_text, detailed_text]
+            )
+            .then(data => {
+                res.send(data.rows)
+            })
+            .catch(err => {
+                res.send({ error: err.message })
+            });
+    } else {
+        res.send({ error: 'Something wrong with auth' })
+    }
 });
 
 module.exports = router;
